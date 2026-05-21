d984ecf614edce0ea66a2336eacd401a1af5a492
InfGithub <Yao_icft@outlook.com>
2026-05-21 20:44:11 +0800

    更新加密模块

--- cryption.py
+++ cryption.py
@@ -6,34 +6,46 @@ def encryption(
     password: bytes,
     buffer: np.ndarray,
     salt_buffer: np.ndarray,
-    chunk_size: int = 1048576
+    mac_buffer: np.ndarray
 ):
-
     salt: bytes = urandom(32)
     salt_buffer[:] = memoryview(salt)
-    password_hashed: bytes = sha256(password).digest()
 
-    for index in range(0, buffer.size, chunk_size):
-        end: int = min(index + chunk_size, buffer.size)
-        chunk_context: bytes = password_hashed + salt + index.to_bytes(8, "big")
-        chunk_keystream: bytes = shake_256(chunk_context).digest(end - index)
-        key_stream: np.ndarray = np.frombuffer(chunk_keystream, dtype=np.uint8)
+    for index in range(0, buffer.size, 1_048_576):
+        end: int = min(index + 1_048_576, buffer.size)
+        chunk_keystream = shake_256()
+        chunk_keystream.update(password)
+        chunk_keystream.update(salt_buffer)
+        chunk_keystream.update(index.to_bytes(length=8))
+        key_stream: np.ndarray = np.frombuffer(chunk_keystream.digest(end - index), dtype=np.uint8)
         buffer[index:end] = np.bitwise_xor(buffer[index:end], key_stream)
+
+    mac = sha256()
+    mac.update(password)
+    mac.update(buffer)
+    mac.update(salt_buffer)
+    mac_buffer[:] = memoryview(mac.digest())
 
 
 def decryption(
     password: bytes,
     buffer: np.ndarray,
     salt_buffer: np.ndarray,
-    chunk_size: int = 1048576
+    mac_buffer: np.ndarray
 ):
+    mac = sha256()
+    mac.update(password)
+    mac.update(buffer)
+    mac.update(salt_buffer)
 
-    password_hashed: bytes = sha256(password).digest()
-    context: bytes = password_hashed + salt_buffer.tobytes()
+    if not np.array_equal(mac_buffer, np.frombuffer(mac.digest(), dtype=np.uint8)):
+        raise ValueError("Warning - Data has been tampered with")
 
-    for index in range(0, buffer.size, chunk_size):
-        end: int = min(index + chunk_size, buffer.size)
-        chunk_context: bytes = context + index.to_bytes(8, "big")
-        chunk_keystream: bytes = shake_256(chunk_context).digest(end - index)
-        key_stream: np.ndarray = np.frombuffer(chunk_keystream, dtype=np.uint8)
+    for index in range(0, buffer.size, 1_048_576):
+        end: int = min(index + 1_048_576, buffer.size)
+        chunk_keystream = shake_256()
+        chunk_keystream.update(password)
+        chunk_keystream.update(salt_buffer)
+        chunk_keystream.update(index.to_bytes(length=8))
+        key_stream: np.ndarray = np.frombuffer(chunk_keystream.digest(end - index), dtype=np.uint8)
         buffer[index:end] = np.bitwise_xor(buffer[index:end], key_stream)
