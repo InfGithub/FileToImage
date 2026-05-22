ab54ba6bdb09b97686404cd96c8f6794fc239e18
InfGithub <Yao_icft@outlook.com>
2026-05-22 20:27:10 +0800

    fix

--- cryption.py
+++ cryption.py
@@ -7,14 +7,15 @@ def cryption(
     buffer: np.ndarray,
     salt_buffer: np.ndarray
 ):
+    base_sponge = shake_256()
+    base_sponge.update(password)
+    base_sponge.update(salt_buffer)
+
     for index in range(0, buffer.size, 1_048_576):
         end: int = min(index + 1_048_576, buffer.size)
-        chunk_keystream = shake_256()
-        chunk_keystream.update(password)
-        chunk_keystream.update(salt_buffer)
-        chunk_keystream.update(index.to_bytes(length=8))
-        key_stream: np.ndarray = np.frombuffer(chunk_keystream.digest(end - index), dtype=np.uint8)
-        buffer[index:end] = np.bitwise_xor(buffer[index:end], key_stream)
+        chunk_sponge = base_sponge.copy()
+        chunk_sponge.update(index.to_bytes(length=8))
+        buffer[index:end] ^= np.frombuffer(chunk_sponge.digest(end - index), dtype=np.uint8)
 
 def encryption(
     password: bytes,
